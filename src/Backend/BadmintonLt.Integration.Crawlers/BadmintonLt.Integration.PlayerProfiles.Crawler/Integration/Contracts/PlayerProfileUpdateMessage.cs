d0228f6e693cb64158928aefc179b45156d786a1
Jevgenij Nekrasov <jevgenij.nekrasov@gmail.com>
2019-01-24 22:15:28 +0200

    Initial PlayerProfilesCrawler

---
+++ src/Backend/BadmintonLt.Integration.Crawlers/BadmintonLt.Integration.PlayerProfiles.Crawler/Integration/Contracts/PlayerProfileUpdateMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace BadmintonLt.Integration.PlayerProfiles.Crawler.Integration.Contracts
+{
+    public class PlayerProfileUpdateMessage
+    {
+        public string Id { get; set; }
+
+        public string ProfileUrl { get; set; }
+
+        public string ClubName { get; set; }
+
+        public string ClubLogoUrl { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
